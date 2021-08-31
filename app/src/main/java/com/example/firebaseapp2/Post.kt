b2bece172a7a33e54b6b277509f9327a6c8cdf51
alifeil <alifiafas@gmail.com>
2021-08-31 13:28:02 +0700

    post

---
+++ app/src/main/java/com/example/firebaseapp2/Post.kt
@@ -0,0 +1,10 @@
+package com.example.firebaseapp2
+
+data class Post (
+    var text: String? = null,
+    var uid: String? = null,
+    var type: String? = null,
+    var postUrl : String? = null,
+    var time: String? = null
+        )
+
