cf95d66bae2d5a1646f1fa25d97f213756b783aa
alifeil <alifiafas@gmail.com>
2021-08-31 14:18:09 +0700

    view holder

---
+++ app/src/main/java/com/example/firebaseapp2/PostViewHolder.kt
@@ -0,0 +1,29 @@
+package com.example.firebaseapp2
+
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import androidx.fragment.app.FragmentActivity
+import androidx.recyclerview.widget.RecyclerView
+
+class PostViewHolder (itemView: View) : RecyclerView.ViewHolder(itemView) {
+    private lateinit var nameTextView: TextView
+    private lateinit var timeTextView: TextView
+    private lateinit var textView: TextView
+    lateinit var deleteButton: Button
+
+    fun setPost(activity: FragmentActivity, post: Post){
+        nameTextView = itemView.findViewById(R.id.postNameTextView)
+        timeTextView = itemView.findViewById(R.id.postTimeTextView)
+        textView = itemView.findViewById(R.id.postTextView)
+        deleteButton = itemView.findViewById(R.id.postDeleteButton)
+
+        nameTextView.text = post.uid
+        timeTextView.text = post.time
+        textView.text = post.text
+
+
+
+        }
+
+}
